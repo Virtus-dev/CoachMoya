8821122865eae4c8f47c730432bfcff88c90732e
isabelchio <isabelchio@hotmail.com>
2022-05-10 17:13:20 +0200

    register changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'age' => 'required|integer|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
@@ -80,6 +82,8 @@ class RegisterController extends Controller
     {
          $user = User::create([
             'name' => $data['name'],
+            'surname' => $data['surname'],
+            'age' => $data['age'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id(); // y si usamos increments?
             $table->string('name');
-            $table->string('apellido')->nullable();
-            $table->integer('edad')->nullable();
+            $table->string('surname');
+            $table->integer('age');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->decimal('RM_Historico_Squat')->nullable();
             $table->decimal('RM_Historico_Bench')->nullable();
             $table->decimal('RM_Historico_DeadLift')->nullable();
